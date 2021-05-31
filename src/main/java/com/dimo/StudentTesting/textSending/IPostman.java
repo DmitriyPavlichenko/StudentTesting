3253d9bfea4d60dd017c3ba93ebefd0aa3643312
Dmitriy Pavlichenko <pavlichenko.dmitriy@gmail.com>
2021-05-31 11:34:08 +0300

    Right answers checking was realized

--- src/main/java/com/dimo/StudentTesting/textSending/IPostman.java
+++ src/main/java/com/dimo/StudentTesting/textSending/IPostman.java
@@ -7,22 +7,30 @@ public interface IPostman {
 
     //Creating string ready to been sent by EMail&Telegram
     default String ListToNormalizedString(List<String> answersList) {
+        final String[] rightAnswers = {"A", "A,B", "A,C", "Rog", "A", "B,C", "Hello"};
+
         if (answersList == null) {
             throw new NullPointerException();
         }
 
         StringBuilder sb = new StringBuilder();
         sb.append("Dmitriy Pavlichenko, TR-91 | Lab7 \n");
-        int counter = 1;
+        int arrayIterator = 0;
+        int rowCounter = 1;
+        int correctCounter = 0;
         for (String i: answersList) {
-            sb.append(counter++).append(")");
+            sb.append(rowCounter++).append(")");
             if (i == null) {
-                sb.append("-");
+                sb.append("<skipped> - wrong");
+            } else if (rightAnswers[arrayIterator++].equals(i)) {
+                sb.append(i).append(" - correct");
+                correctCounter++;
             } else {
-                sb.append(i);
+                sb.append(i).append(" - wrong, correct variant: ").append(rightAnswers[arrayIterator - 1]);
             }
             sb.append("\n");
         }
+        sb.append("Total number of correct answers: ").append(correctCounter).append("\n");
         return sb.toString();
     }
 }
